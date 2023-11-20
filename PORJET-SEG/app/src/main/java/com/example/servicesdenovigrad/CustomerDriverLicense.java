6f7c37dcd0766db0ab7e11f34ce09104d7b6b844
unknown <hkala042@uottawa.ca>
2023-11-19 23:28:18 -0500

    Livrable2 start

---
+++ PORJET-SEG/app/src/main/java/com/example/servicesdenovigrad/CustomerDriverLicense.java
@@ -0,0 +1,14 @@
+package com.example.servicesdenovigrad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerDriverLicense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_driver_license);
+    }
+}
---
+++ PORJET-SEG/app/src/main/java/com/example/servicesdenovigrad/CustomerParkingPermit.java
@@ -0,0 +1,14 @@
+package com.example.servicesdenovigrad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerParkingPermit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_parking_permit);
+    }
+}
